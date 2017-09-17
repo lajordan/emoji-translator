e813c73fad7b7e957b8175a06fa2ead04d853930
lajordan <lajordan@mit.edu>
2017-09-17 02:47:40 -0400

    stuck some stuff in text_to_translate

--- translate.py
+++ translate.py
@@ -2,7 +2,6 @@
 # yo
 # hello world
 # get it cuz you're my world
-
 import re
 from unicode_dict import EMOJI_UNICODE
 
@@ -10,18 +9,23 @@ from unicode_dict import EMOJI_UNICODE
 smiley = "\U0001f600"
 frowny = "\U00002639"
 
-input_text = input("What do you want to emojify?\n")
+# input_text = input("What do you want to emojify?\n")
+input_text = open("text_to_translate.txt", "r").read()
+
 
 def get_emoji(word):
     word = word.group(0)
     word_alphanum = re.sub(r"[^a-zA-Z0-9_]", "", word)
-    if word_alphanum.lower() in EMOJI_UNICODE.keys():
-        return re.sub(r"([\w]+)", EMOJI_UNICODE[word_alphanum.lower()] + " ", word)
-    # Handle plurals
-    elif word_alphanum[-1]=="s" and word_alphanum.lower()[:-1] in EMOJI_UNICODE.keys():
-        return re.sub(
-            r"([\w]+)", EMOJI_UNICODE[word_alphanum.lower()[:-1]]*2 + " ", word
-            )
+    if word_alphanum != "":
+        if word_alphanum.lower() in EMOJI_UNICODE.keys():
+            return re.sub(r"([\w]+)", EMOJI_UNICODE[word_alphanum.lower()] + " ", word)
+        # Handle plurals
+        elif word_alphanum[-1]=="s" and word_alphanum.lower()[:-1] in EMOJI_UNICODE.keys():
+            return re.sub(
+                r"([\w]+)", EMOJI_UNICODE[word_alphanum.lower()[:-1]]*2 + " ", word
+                )
+        else:
+            return word
     else:
         return word
 
